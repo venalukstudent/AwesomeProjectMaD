59c6ad87949ac740c08cff49e7bd534f01275d8b
Venal <s22310263@student.unklab.ac.id>
2025-09-01 21:53:40 +0800

    Exercise 2

--- App.tsx
+++ App.tsx
@@ -1,118 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, StyleSheet, View, Image} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//Komponen adalah fungsi yg mengembalikan JSX
+const App = () => {
+  // Contoh string base64 (gambar kecil kotak hitam)
+  const base64Image =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPj/HwADBwIAMCbHYQAAAABJRU5ErkJggg==';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Basic React Native</Text>
+      </View>
+      {/* Menampilkan gambar base64 */}
+      <Image source={require('./assets/P1160403.jpg')} style={styles.image} />
+
+      <Image
+        source={{
+          uri: 'https://www.unklab.ac.id/wp-content/uploads/2023/01/pic-for-sejarah-300x231.jpg',
+        }}
+        style={styles.image}
+      />
+
+      {/* Menampilkan gambar dari Base64 */}
+      <Image
+        source={{
+          uri: base64Image,
+        }}
+        style={styles.image}
+      />
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  title: {
+    backgroundColor: 'yellow',
+    color: 'red',
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  container: {
+    backgroundColor: 'blue',
+    borderColor: 'black',
+    borderWidth: 5,
+    padding: 15,
+    margin: 15,
+  },
+
+  title2: {
+    backgroundColor: 'green',
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  container2: {
+    backgroundColor: 'red',
+    borderColor: 'blue',
+    borderWidth: 5,
+    width: 100,
+    height: 100,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    margin: 10,
+    borderRadius: 10,
+  },
+});
